3708cebaa3e7db2b507087a764e825c235c1baf3
JaDogg <jadogg.coder@gmail.com>
2014-07-31 17:05:00 +0530

    New Test Cases

--- src/test/java/info/simpll/simplematch/SimpleMatchTest.java
+++ src/test/java/info/simpll/simplematch/SimpleMatchTest.java
@@ -20,30 +20,47 @@ public class SimpleMatchTest {
         SimpleMatch m2 = new SimpleMatch("a*b", "abj");
         Assert.assertFalse(m2.match());
     }
+
     @Test
     public void test3() {
         SimpleMatch m3 = new SimpleMatch("a?b", "acd");
         Assert.assertFalse(m3.match());
     }
+
     @Test
     public void test4() {
         SimpleMatch m4 = new SimpleMatch("a*", "abcdefg");
         Assert.assertTrue(m4.match());
     }
+
     @Test
     public void test5() {
         SimpleMatch m5 = new SimpleMatch("a*ba", "abba");
         Assert.assertTrue(m5.match());
 
     }
+
     @Test
     public void test6() {
         SimpleMatch m6 = new SimpleMatch("avenra", "avenra");
         Assert.assertTrue(m6.match());
     }
+
     @Test
     public void test7() {
         SimpleMatch m7 = new SimpleMatch("a?", "a1");
         Assert.assertTrue(m7.match());
     }
+
+    @Test
+    public void test8() {
+        SimpleMatch m8 = new SimpleMatch("bhathiya", "blah");
+        Assert.assertFalse(m8.match());
+    }
+    
+    @Test
+    public void test9() {
+        SimpleMatch m9 = new SimpleMatch("/img/abc.jpg", "/img/abc.jpg");
+        Assert.assertTrue(m9.match());
+    }    
 }
